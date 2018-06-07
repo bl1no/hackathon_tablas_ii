c380f2454b11452d121b2954bd623bcc8693cdb9
Julian Perez <julian.perez@bbva.com>
2018-06-07 08:54:24 +0200

    added training with params

--- back/py_app/service/predictor.py
+++ back/py_app/service/predictor.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 
 data_file_pattern = '/*'
 data_trained_model = '/trainning/output.model'
+data_trained_filters_model = '/trainning/outputfilters.model'
 
 def trainning(files_path):
     files = glob.glob(files_path + data_file_pattern)
@@ -25,7 +26,7 @@ def trainning(files_path):
 
     procesed_dataframe.to_csv(data_trained_model, sep='\t', encoding='utf-8', index=False)
 
-def dynamic_trainning(files_path, filters):
+def dynamic_trainning(files_path, filters, country, personType, globalSegment):
     files = glob.glob(files_path + data_file_pattern)
 
     list_of_dataframes = [pd.read_table(filename,  sep='|', encoding = "ISO-8859-1") for filename in files]
@@ -36,9 +37,9 @@ def dynamic_trainning(files_path, filters):
 
     combined_dataframes = pd.concat(list_of_processed_dataframes, ignore_index=True)
 
-    procesed_dataframe = dynamic_group_by(combined_dataframes,filters)
+    procesed_dataframe = dynamic_group_by(combined_dataframes,filters,country,personType,globalSegment)
 
-    procesed_dataframe.to_csv(data_trained_model, sep='\t', encoding='utf-8', index=False)
+    procesed_dataframe.to_csv(data_trained_filters_model, sep='\t', encoding='utf-8', index=False)
 
 def clean_data(dataframe):
     dataframe = dataframe.drop(axis=1, index=0)
@@ -52,12 +53,22 @@ def group_by_day_hour(dataframe):
     dataframe = dataframe.groupby(['criteria']).size().reset_index(name='count')
     return dataframe[['criteria','count']]
 
-def dynamic_group_by(dataframe, filters):
+def dynamic_group_by(dataframe, filters, country, personType, globalSegment):
     dataframe = dataframe.rename(columns={' FECHA_HORA_INICIO_LLAMADA ': 'end_call', ' FECHA_HORA_FIN_LLAMADA ': 'start_call'})
-    dataframe = dataframe[["start_call"]]
+    dataframe = dataframe[filters + ["start_call"]]
+
     dataframe['criteria'] = dataframe.start_call.apply(lambda x: datetime.strptime(x.strip(), "%Y-%m-%d %H:%M:%S").replace(microsecond=0,second=0,minute=0))
     dataframe['criteria'] = dataframe.criteria.apply(lambda x: x.strftime('%Y-%m-%d %H'))
+
     dataframe = dataframe.groupby(filters + ['criteria']).size().reset_index(name='count')
+
+    if (country):
+        dataframe = dataframe.loc[(dataframe['  PAIS  '].str.strip() == country)]
+    elif (personType):
+        dataframe = dataframe.loc[(dataframe[' FISICA_JURIDICA '].str.strip() == personType)]
+    elif (globalSegment):
+        dataframe = dataframe.loc[(dataframe['         SEGMENTO_GLOBAL          '].str.strip() == globalSegment)]
+
     return dataframe[['criteria','count']]
 
 def predict_phoncalls_date(timestamp):
